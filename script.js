36f8f330118dee2b40b664e1c607ba0d36c86edc
Luis Medina G <humberto.medina@alumno.udg.mx>
2021-03-27 08:12:43 +0000

    adding lives

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const gravity = 1400;
+const jumpForce = 500;
+const playerSpeed = 350;
+
 var config = {
     type: Phaser.AUTO,
     width: 860,
@@ -6,7 +10,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 650 },
+            gravity: { y: gravity },
             debug: false
         }
     },
@@ -17,107 +21,88 @@ var config = {
     }
 };
 
-
+var lives;
 var player;
-var stars;
 var bombs;
-var platforms;
 var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
+var scoreLives;
 
-var wals;
+var total = 100;
+
+var walls;
 var coins;
-var enemies;
+var spikes;
 
 var level = [
-    'xxxxxxxxxxxxxxxxxxxxxxxxx',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'x                       x',
-    'xxxxxxxxxxxxxxxxxxxxxxxxx',
+    'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'x  o                     x',
+    'x  xxx    o   xxxx!xxxx! x',
+    'x     x   x       !      x',
+    'xo                !     xx',
+    'xx    o                  x',
+    'x     x                 xx',
+    'xx          o    !   o ! x',
+    'xo         xxx       x   x',
+    'xx  !!  !!       o       x',
+    'x                x       x',
+    'xx                       x',
+    'xo         o             x',
+    'xxxxx     xxx !!! xx     x',
+    'x      !             x   x',
+    'x  o                 !   x',
+    'x xxx                   xx',
+    'x        !      o   xxx !x',
+    'x  o  !  o  !  xxx!  o  !x',
+    'xxxxxxxxxxxxxxxxxxxxxxxxxx',
 ];
 
 var game = new Phaser.Game(config);
+
+function createLevel() {
+    for (var i = 0; i < level.length; i++) {
+        for (var j = 0; j < level[i].length; j++) {
+            if (level[i][j] == 'x') {
+                walls.create(32 + 32 * j, 32 + 32 * i, 'wall').setScale(0.5).refreshBody();
+            } else if (level[i][j] == 'o') {
+                coins.create(32 + 32 * j, 32 + 32 * i - 15, 'coin').setScale(0.5);
+            } else if (level[i][j] == '!') {
+                spikes.create(32 + 32 * j, 32 + 32 * i, 'enemy').setScale(0.5).refreshBody();
+            }
+        }
+    }
+}
 
 function preload() {
     this.load.image('wall', 'assets/wall.png');
     this.load.image('enemy', 'assets/enemy.png');
     this.load.image('coin', 'assets/coin.png');
-
-    // this.load.image('ground', 'assets/platform.png');
-    // this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-
     this.load.spritesheet('dude',
         'assets/dudes.png',
         { frameWidth: 11, frameHeight: 15 }
     );
 }
 
+
 function create() {
-    platforms = this.physics.add.staticGroup();
+    lives = 3;
+    score = 0;
 
-    // ================================================================================
     walls = this.physics.add.staticGroup();
-    coins = this.physics.add.staticGroup();
-    enemies = this.physics.add.staticGroup();
+    spikes = this.physics.add.staticGroup();
 
-    for (var i = 0; i < level.length; i++) {
-        for (var j = 0; j < level[i].length; j++) {
+    coins = this.physics.add.group();
+    bombs = this.physics.add.group();
 
-            // Create a wall and add it to the 'walls' group
-            if (level[i][j] == 'x') {
-                walls.create(32 + 32 * j, 32 + 32 * i, 'wall').setScale(0.5).refreshBody();
-            }
+    createLevel();
 
-            // Create a coin and add it to the 'coins' group
-            else if (level[i][j] == 'o') {
-                var coin = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'coin');
-                coins.add(coin);
-            }
-
-            // Create a enemy and add it to the 'enemies' group
-            else if (level[i][j] == '!') {
-                var enemy = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'bomb');
-                enemies.add(enemy);
-            }
-        }
-    }
-
-    // ================================================================================
-
-    // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    // platforms.create(600, 400, 'ground');
-    // platforms.create(50, 250, 'ground');
-    // platforms.create(750, 220, 'ground');
-
-    // movingPlatform = this.physics.add.image(400, 400, 'ground');
-
-    // movingPlatform.setImmovable(true);
-    // movingPlatform.body.allowGravity = false;
-    // movingPlatform.setVelocityX(50);
-
-    player = this.physics.add.sprite(32 * 3.5, 32 * 15, 'dude');
+    player = this.physics.add.sprite(54, 32 * 18, 'dude');
 
     player.setBounce(0.1);
     player.setCollideWorldBounds(true);
-    player.setGravityY(500);
 
     this.anims.create({
         key: 'left',
@@ -141,86 +126,55 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    // stars = this.physics.add.group({
-    //     key: 'star',
-    //     repeat: 10,
-    //     setXY: { x: 12, y: 0, stepX: 70 }
-    // });
-
-    // stars.children.iterate(function (child) {
-
-    //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    // });
-
-    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '16px', fill: '#000' });
-
-    // bombs = this.physics.add.group();
+    scoreText = this.add.text(24, 24, 'Score: 0', { fontSize: '16px', fill: '#000' });
+    scoreLives = this.add.text(124, 24, 'Lives: 0', { fontSize: '16px', fill: '#000' });
+    scoreLives.setText('Lives: ' + lives);
 
     this.physics.add.collider(player, walls);
+    this.physics.add.collider(coins, walls);
+    this.physics.add.collider(bombs, walls);
 
-    // this.physics.add.collider(player, platforms);
-    // this.physics.add.collider(player, movingPlatform);
-    // this.physics.add.collider(stars, platforms);
-    // this.physics.add.collider(stars, movingPlatform);
-    // this.physics.add.collider(bombs, platforms);
-
-    // this.physics.add.overlap(player, stars, collectStar, null, this);
-
-    // this.physics.add.collider(player, bombs, hitBomb, null, this);
+    this.physics.add.collider(player, coins, collectCoin, null, this);
+    this.physics.add.collider(player, spikes, hitBomb, null, this);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update() {
-    // if (gameOver) {
-    //     return;
-    // }
+    if (cursors.left.isDown) {
+        player.setVelocityX(-playerSpeed);
 
-    // if (cursors.left.isDown) {
-    //     player.setVelocityX(-330);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(playerSpeed);
 
-    //     player.anims.play('left', true);
-    // }
-    // else if (cursors.right.isDown) {
-    //     player.setVelocityX(330);
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
 
-    //     player.anims.play('right', true);
-    // }
-    // else {
-    //     player.setVelocityX(0);
+        player.anims.play('turn');
+    }
 
-    //     player.anims.play('turn');
-    // }
-
-    // if (cursors.up.isDown && player.body.touching.down) {
-    //     player.setVelocityY(-650);
-    // }
-
-    // if (movingPlatform.x >= 500) {
-    //     movingPlatform.setVelocityX(-60);
-    // }
-    // else if (movingPlatform.x <= 300) {
-    //     movingPlatform.setVelocityX(60);
-    // }
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-jumpForce);
+    }
 }
 
-function collectStar(player, star) {
-    star.disableBody(true, true);
+function collectCoin(player, coin) {
+    coin.disableBody(true, true);
 
-    //  Add and update the score
     score += 10;
     scoreText.setText('Score: ' + score);
 
-    console.log("Logging test");
-    if (stars.countActive(true) === 0) {
-        console.log("No stars");
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
-            child.enableBody(true, child.x, 0, true, true);
+    if (coins.countActive(true) === 0) {
+        coins.children.iterate(function (child) {
+            child.enableBody(true, child.x, child.y - 20, true, true);
         });
 
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        var x = Phaser.Math.Between(54, 626);
 
-        var bomb = bombs.create(x, 16, 'bomb');
+        var bomb = bombs.create(x, 54, 'bomb');
         bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
@@ -229,8 +183,15 @@ function collectStar(player, star) {
 }
 
 function hitBomb(player, bomb) {
-    this.physics.pause();
-    player.setTint(0xff0000);
-    player.anims.play('turn');
-    gameOver = true;
+    if (lives === 0) {
+        this.physics.pause();
+        player.setTint(0xff0000);
+
+        this.registry.destroy();
+        this.events.off();
+        this.scene.restart();
+    } else {
+        lives--;
+        scoreLives.setText('Lives: ' + lives);
+    }
 }
